f8201da88d00951a6f03fb18dfe9422f1261a2f1
GavinHome <yangxiaoming512@yeah.net>
2019-09-05 17:41:34 +0800

    修改bug

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,11 @@ import * as https from 'https';
 async function run() {
   try {
     var token = core.getInput('token');
-    token = token ? token : 'f8e6693c61f3a970efe789133f5dd4b691ab9a6f0f9058022bf450057a523713'
     if (!token) {
       core.setFailed('dingding token is required!');
     }
 
     var body: any = core.getInput('body');
-    body = body ? body : JSON.stringify({
-      "msgtype": "link",
-      "link": {
-        "text": "test",
-        "title": "test",
-        "picUrl": "",
-        "messageUrl": "https://www.dingtalk.com/s?__biz=MzA4NjMwMTA2Ng==&mid=2650316842&idx=1&sn=60da3ea2b29f1dcc43a7c8e4a7c97a16&scene=2&srcid=09189AnRJEdIiWVaKltFzNTw&from=timeline&isappinstalled=0&key=&ascene=2&uin=&devicetype=android-23&version=26031933&nettype=WIFI"
-      }
-    })
 
     if (!body) {
       core.setFailed('post body is required!');
@@ -27,12 +17,6 @@ async function run() {
     if (!token || !body) {
       return;
     }
-
-    console.log(`token: ${token}`);
-    core.debug(`token: ${token}`);
-    console.log(`body: ${body}`);
-    console.log(`body json: ${body}`);
-    core.debug(`body: ${body}`);
 
     const req = https.request({
       protocol: 'https:',
@@ -43,25 +27,11 @@ async function run() {
       headers: {
         'Content-Type': "application/json; charset=utf-8"
       }
-    }, (res) => {
-      core.debug(`状态码: ${res.statusCode}`);
-      console.log(`状态码: ${res.statusCode}`);
-      core.debug(`响应头: ${JSON.stringify(res.headers)}`);
-      console.log(`响应头: ${JSON.stringify(res.headers)}`);
-      res.setEncoding('utf8');
-      res.on('data', (chunk) => {
-        console.log(`响应主体: ${chunk}`);
-        core.debug(`响应主体: ${chunk}`);
-      });
-      res.on('end', () => {
-        console.log('响应中已无数据。');
-        core.debug('响应中已无数据。');
-      });
     });
 
     req.on('error', function (err) {
       core.setFailed('post message is failed!');
-      console.error(err);
+      console.error('post message is failed! error: ' + err);
     });
 
     req.write(body);
